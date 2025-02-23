395e4490c8e12a07804746dfc37683c8427d6d5e
Monika <monik.stefanova@gmail.com>
2025-02-24 00:41:20 +0100

    update

--- VITE-BOILERPLATE/src/hello.tsx
+++ VITE-BOILERPLATE/src/hello.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
 export const HelloComponent: React.FC = () => {
-  const name = "Monika";
-
   return (
     <>
-      <h1>Hello from React, {name}!</h1>
+      <h1>Hello from React!</h1>
     </>
   );
 };
--- VITE-BOILERPLATE/src/main.tsx
+++ VITE-BOILERPLATE/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { HelloComponent } from "./hello";
+import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
 
